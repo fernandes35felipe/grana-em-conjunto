ae00ec875d9aed906df36b9d1b6b8a77651d921a
fernandes35felipe <fernandes35felipe@gmail.com>
2025-10-30 18:13:30 -0300

    ajustando bundle sizing

---
+++ src/lib/date.ts
@@ -0,0 +1,16 @@
+export {
+  format,
+  startOfMonth,
+  endOfMonth,
+  startOfYear,
+  endOfYear,
+  subMonths,
+  addMonths,
+  isAfter,
+  isBefore,
+  parseISO,
+  addDays,
+  subDays,
+} from "date-fns";
+
+export { ptBR } from "date-fns/locale";
